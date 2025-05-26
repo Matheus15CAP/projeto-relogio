714b48d7a2e0f3c0f934ad45a809578f8dfdfcc9
Matheus (M6) <133786715+Matheus15CAP@users.noreply.github.com>
2025-05-26 10:41:48 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const botoes = document.querySelectorAll (".botoes");
 console.log(botoes)
 for(let i=0;i<botoes.length; i++) {
-    console.log(i);
+    botoes[i].onclick = function () {
+        botoes[i].classList.add("ativo");
+    }
 }
