d13316abdd3d6889a167460b726bc0b4a66aa8bc
Matheus (M6) <133786715+Matheus15CAP@users.noreply.github.com>
2025-06-23 10:43:15 -0300

    .

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botoes");
 const textos = document.querySelectorAll(".abas-conteudo");
+const contadores = document.querySelectorAll(".contador");
 console.log(botoes)
 for(let i=0; i< botoes.length; i++){
     botoes[i].onclick = function(){
